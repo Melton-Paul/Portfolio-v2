6c1d30b55f4d03e4da36184933c458ad498a1864
Paul Melton <melton.paul@hotmail.com>
2022-08-16 20:50:46 -0500

    SA optimizer,  removed unused code, added redirect

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import styles from "./About.module.css";
-import TechnologyList from "../TechnologyList/TechnologyList";
-import Projects from "../Projects/Projects";
 import Button from "../../UI/Button/Button";
-import { Link } from "react-router-dom";
 
 export default function About() {
   return (
     <article className={styles.about}>
       <p className={styles["about--greeting"]}>
-        My name is Paul Melton. I am a self-taught Front-End Developer, with a
-        passion for clean, reusable, and adaptable code. <br /> <br />I am open
-        to learning new languages or adapting to the needs of a team- and being
-        that I am self taught, means I can pick up languages on the fly! <br />{" "}
-        <br />
+        <h1 className={styles["SA-optimizer"]}>My name is Paul Melton. </h1>
+        I am a self-taught Front-End Developer, with a passion for clean,
+        reusable, and adaptable code. <br /> <br />I am open to learning new
+        languages or adapting to the needs of a team- and being that I am self
+        taught, means I can pick up languages on the fly! <br /> <br />
         For more information on me, check out my LinkedIn
       </p>
       <Button
